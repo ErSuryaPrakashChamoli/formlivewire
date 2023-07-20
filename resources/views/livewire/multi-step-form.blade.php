9cfbdcca880e414a1d022cfff97d75e22177e694
suryaprakashchamoli <suryaprakashchamoli@gmail.com>
2023-07-20 12:24:48 +0530

    form form basic layout ready for modify only multi step form view

--- resources/views/livewire/multi-step-form.blade.php
+++ resources/views/livewire/multi-step-form.blade.php
@@ -2,7 +2,7 @@
     <div class="step-one">
         <div class="card">
             <div class="card-header bg-secondary text-white">
-              setp 1/4 -personal detail
+                setp 1/4 -personal detail
             </div>
             <div class="card-body">
                 <div class="row">
@@ -17,9 +17,9 @@
                             <label for="">Last Name</label>
                             <input id="" class="form-control" type="text" name="" placeholder="Enter Last name">
                         </div>
-                    </div> 
+                    </div>
                 </div>
-                 <div class="row">
+                <div class="row">
                     <div class="col-md-6">
                         <div class="form-group">
                             <label for="gender">Genger</label>
@@ -37,13 +37,113 @@
                             <input id="my-input" class="form-control" type="text" name="" placeholder="enter age">
                         </div>
                     </div>
-                 </div>
-                 <div class="form-group">
-                        <label>Description</label>
-                        <textarea  class="form-control" name="" id="" cols="2" rows="2"></textarea>
-                    </div>
+                </div>
+                <div class="form-group">
+                    <label>Description</label>
+                    <textarea class="form-control" name="" id="" cols="2" rows="2"></textarea>
+                </div>
 
             </div>
         </div>
     </div>
+
+
+    <!-- step 2 -->
+
+
+    <div class="step-2">
+        <div class="card">
+            <div class="card-header bg-secondary text-white">Step 2/4 - Address & Contacts</div>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="my-input">Email Address</label>
+                            <input id="my-input" class="form-control" type="text" name="" placeholder="Enter Email Address">
+                        </div>
+                    </div>
+
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="my-input">Phone</label>
+                            <input id="my-input" class="form-control" type="text" name="" placeholder="Enter Phone Number">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="my-input">country of registance</label>
+                            <select name="" id="" class="form-group">
+                                <option value="" selected>Select Country</option>
+                                <option value="United State">United State</option>
+                                <option value="India">India</option>
+                                <option value="Pakistan">Pakistan</option>
+                                <option value="London">London</option>
+                            </select>
+
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="">City</label>
+                            <input id="my-input" class="form-control" type="text" name="" placeholder="Enter your city">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+        <!-- step 3 -->
+
+        <div class="step-three">
+            <div class="card">
+                <div class="card-header bg-secondary text-white">Step 3/4 -framworks experience</div>
+                <div class="card-body">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores earum, et fuga laboriosam officiis velit enim nihil magnam similique ratione inventore eligendi cupiditate iure voluptates ex blanditiis vel. Labore, enim?
+
+                    <div class="framworks d-flex flex-column align-items-left mt-2">
+                        <label for="laravel">
+                        <input type="checkbox" name="laravel" id="laravel">Laravel
+                        </label>
+                    
+                        <label for="cogniter"><input type="checkbox" name="Cogniter" id="Cogniter">Cogniter</label>
+
+                        <label for="vuejs">
+                        <input type="checkbox" name="vuejs" id="vuejs">vuejs
+                        </label>
+
+                        <label for="cakephp">
+                        <input type="checkbox" name="Cake Php" id="Cake Php">Cake Php
+                        </label> 
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <!-- step 4 -->
+
+        <div class="step-four">
+            <div class="card">
+                <div class="card-header bg-secondary text-white">Step 4/4 - Attachemente</div>
+                <div class="card-body">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores consectetur sapiente doloremque, neque quo fugit eius accusamus blanditiis quae, dolore excepturi eum quam esse eveniet illo voluptatibus voluptatum. Tempore, quos?
+                    <div class="form-group">
+                        <label for="CV">CV</label>
+                        <input id="my-input" class="form-control" type="file" name="">
+                    </div>
+                    <div class="form-group">
+                        <label for="terms" class="d-block">
+                            <input type="checkbox" name="" id="terms">You Must agree with our <a href="#">Terms and Conditions</a>
+                        </label>
+                    </div>
+                </div>
+            </div>
+        </div>
+
 </div>
